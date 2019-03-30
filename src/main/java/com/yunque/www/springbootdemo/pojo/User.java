c58e8529f1a8ebda2f52bde76d4ba9a1d14ff31f
rushui.chen <1194320692@qq.com>
2019-03-30 18:15:46 +0800

    “20190330 jsckson序列化与反序列化

--- src/main/java/com/yunque/www/springbootdemo/pojo/User.java
+++ src/main/java/com/yunque/www/springbootdemo/pojo/User.java
@@ -1,6 +1,9 @@
 package com.yunque.www.springbootdemo.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.util.Date;
 
 //@JsonInclude(JsonInclude.Include.NON_NULL)
 //可以在主配置文件中进行统一的配置，值为null的进行统一过滤
@@ -9,6 +12,7 @@ public class User {
 
     private String name;
     private String password;
+    private Date birthday;
 
 
 }
---
+++ src/main/java/com/yunque/www/springbootdemo/utils/JacksonUtils.java
@@ -0,0 +1,58 @@
+package com.yunque.www.springbootdemo.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import com.yunque.www.springbootdemo.pojo.User;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created on 2019/3/30.
+ * author:crs
+ * Description:工具类
+ * SimpleDateFormat类的使用
+ * this(System.currentTimeMillis()); 内部源码，返回系统时间的毫秒值
+ * 如果是反序列化，要知道反序列化的类型，是集合还是自定义class
+ */
+
+public class JacksonUtils {
+
+    public static void main(String[] args) throws Exception {
+        User user = new User();
+        user.setName("crs");
+        user.setPassword("1111");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        //user.setBirthday(format.parse("1996-10-01"));
+        user.setBirthday(new Date());
+
+        ObjectMapper mapper = new ObjectMapper();
+        //todo：设置时间序列化的格式:仅仅是序列化
+        //mapper.setDateFormat(format);
+
+        //对象转json
+        String result = mapper.writeValueAsString(user);
+        System.out.println(result);
+        //集合转json
+        ArrayList<User> list = new ArrayList<>();
+        list.add(user);
+        String resULTList = mapper.writeValueAsString(list);
+        System.out.println(resULTList);
+
+        String str="{\"name\":\"crs\",\"password\":\"1111\",\"birthday\":1553939852568}";
+        //把字符串反序列化成class
+        mapper.setDateFormat(format);
+        User resultUser = mapper.readValue(str, User.class);
+        Date birthday = resultUser.getBirthday();
+        resultUser.setBirthday(birthday);
+        System.out.println(resultUser.toString());
+
+        //todo：new Date().getTime() 获取当前时间的毫秒值进行打印
+        System.out.println(new Date().getTime());
+
+    }
+
+
+}
